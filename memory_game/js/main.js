67ed29b8afdb2038fa0f12264914f60e4d85eac8
Stanley Sham <stanley.sham94@gmail.com>
2019-06-30 19:05:51 -0400

    created game log

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+// Creating values for cards
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king"
+var cardFour = "queen";
+
+// Creating console to say it flipped card
+console.log("User has flipped " + cardOne);
+console.log("User has flipped " + cardTwo);
