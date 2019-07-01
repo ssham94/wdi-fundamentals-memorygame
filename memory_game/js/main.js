952e7b6fa324f16f768eb228e04dbd69b5d73a33
Stanley Sham <stanley.sham94@gmail.com>
2019-06-30 20:17:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,29 @@
 console.log("Up and running!");
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+
 // Creating values for cards
-var cardOne = "king";
-var cardTwo = "queen";
+var cardOne = cards[0];
+var cardTwo = cards[2];
 var cardThree = "king"
 var cardFour = "queen";
 
-// Creating console to say it flipped card
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User has flipped " + cardOne);
 console.log("User has flipped " + cardTwo);
+
+
+//Creating the check for the match
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else if (cardsInPlay[0] !== cardsInPlay[1]){
+		alert("Sorry try again");
+	}
+}
